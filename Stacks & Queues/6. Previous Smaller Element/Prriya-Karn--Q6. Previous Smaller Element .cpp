a2fe20bac0887fa0b20379327e40e05cc25e5ee7
Prriya-Karn <101259927+Prriya-Karn@users.noreply.github.com>
2026-01-25 16:21:57 +0000

    solved stack q6 previous smaller element

---
+++ Stacks & Queues/6. Previous Smaller Element/Prriya-Karn--Q6. Previous Smaller Element .cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int>& arr) {
+        //  code here
+        stack<int> st;
+        vector<int> res;
+        
+        for(int i=0;i<arr.size();i++){
+            while(!st.empty() && st.top()>=arr[i]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                res.push_back(-1);
+                st.push(arr[i]);
+            }else if(st.top()<arr[i]){
+                res.push_back(st.top());
+                st.push(arr[i]);
+            }
+        }
+        
+        return res;
+        
+    }
+};
+
+
+
