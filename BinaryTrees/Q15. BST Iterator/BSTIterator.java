82cf9bd579a7015566eba9d8fbb239e349c47fbf
Megha Mohata <155289236+mohatamegha@users.noreply.github.com>
2026-01-29 21:54:59 +0530

    Implement BSTIterator for in-order traversal

---
+++ BinaryTrees/Q15. BST Iterator/BSTIterator.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> st = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushInStack(root);
+    }
+    
+    public void pushInStack(TreeNode root){
+        if(root == null)
+            return;
+        st.push(root);
+        pushInStack(root.left);        
+    }
+
+    public int next() {
+        TreeNode node = st.pop();
+        pushInStack(node.right);
+        return node.val;
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
