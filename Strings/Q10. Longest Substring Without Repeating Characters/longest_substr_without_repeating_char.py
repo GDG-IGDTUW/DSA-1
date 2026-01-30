48ed5daa1dabaf6b14d88eae789424ef64462d3b
Tanya Gupta <tanya441g@gmail.com>
2026-01-30 22:05:22 +0530

    longest_substr_without_repeating_char.py

--- Strings/Q10. Longest Substring Without Repeating Characters/longest_substr_without_repeating_char.py
+++ Strings/Q10. Longest Substring Without Repeating Characters/longest_substr_without_repeating_char.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        hash = [-1] * 256 
-        l = 0
+        hash=[-1] * 256 
+        l=0
         maxLen = 0
         for r in range(len(s)):
-            if hash[ord(s[r])] != -1:
-                if hash[ord(s[r])] >= l:
-                    l = hash[ord(s[r])] + 1 
-            hash[ord(s[r])] = r
-            length = r - l + 1
+            if hash[ord(s[r])]!=-1:
+                if hash[ord(s[r])]>=l:
+                    l = hash[ord(s[r])]+1 
+            hash[ord(s[r])]=r
+            length = r-l+1
             maxLen = max(maxLen, length)
         return maxLen
+
