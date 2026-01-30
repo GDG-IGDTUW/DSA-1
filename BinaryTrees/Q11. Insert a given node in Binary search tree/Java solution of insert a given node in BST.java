fe7535148787693b3bb4500e0ed1fa446d351dcf
Khushi <khushisarojj@gmail.com>
2026-01-30 14:18:38 +0530

    Java solution of insert a given node in BST.java

---
+++ BinaryTrees/Q11. Insert a given node in Binary search tree/Java solution of insert a given node in BST.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // If tree is empty, create new node
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        // If val < root, insert into left subtree
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } 
+        // If val > root, insert into right subtree
+        else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root; 
+    }
+}
