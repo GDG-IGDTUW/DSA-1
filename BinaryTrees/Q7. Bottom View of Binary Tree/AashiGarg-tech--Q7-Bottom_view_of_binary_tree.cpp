e3b9635cb3aff55b503c515a6262ca23a9394311
Aashi Garg <aashigarg2504@gmail.com>
2026-02-06 20:11:00 +0530

    Binary Trees - Q7 Bottom View Of Binary Tree - cpp

---
+++ BinaryTrees/Q7. Bottom View of Binary Tree/AashiGarg-tech--Q7-Bottom_view_of_binary_tree.cpp
@@ -0,0 +1,53 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+    public:
+      vector<int> bottomView(Node *root) {
+          vector<int> ans;
+          if (!root) return ans;
+  
+          // horizontal distance -> node value
+          map<int, int> mp;
+  
+          // queue stores node and its horizontal distance
+          queue<pair<Node*, int>> q;
+          q.push({root, 0});
+  
+          while (!q.empty()) {
+              auto curr = q.front();
+              q.pop();
+  
+              Node* node = curr.first;
+              int hd = curr.second;
+  
+              // overwrite so bottom-most remains
+              mp[hd] = node->data;
+  
+              if (node->left)
+                  q.push({node->left, hd - 1});
+  
+              if (node->right)
+                  q.push({node->right, hd + 1});
+          }
+  
+          // collect result from leftmost to rightmost HD
+          for (auto &x : mp) {
+              ans.push_back(x.second);
+          }
+  
+          return ans;
+      }
+  };
+  
