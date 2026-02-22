4fb3b0e2ef613f0adeb8b310957a33fdc159ea6f
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2026-02-22 10:26:56 +0530

    Implement bipartition check in solution.py

---
+++ Graphs/Q15. All Paths From Source to Target/solution.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: list[list[int]]) -> bool:
+       
+        graph = defaultdict(list)
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        color = [0] * (n + 1)
+  
+        for person in range(1, n + 1):
+            if color[person] != 0:
+                continue
+            
+            queue = deque([person])
+            color[person] = 1
+            
+            while queue:
+                current = queue.popleft()
+                
+                for neighbor in graph[current]:
+                    if color[neighbor] == 0:
+                        color[neighbor] = -color[current]
+                        queue.append(neighbor)
+                    elif color[neighbor] == color[current]:
+                        return False
+        
+        return True
